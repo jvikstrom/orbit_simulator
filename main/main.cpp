10631ce26a3ed838a68f1dcd86d9e5a7f03d55b6
Johan Vikström <tritzium@gmail.com>
2017-05-31 23:39:47 +0200

    Added GUI module

--- main/main.cpp
+++ main/main.cpp
@@ -7,7 +7,7 @@ int main(){
   simulator sim(0, end, 0.1);
   object earth(vec2(0,0), vec2(0,0), 5000000);
   earth.name = "earth";
-  object obj(vec2(1, 0), vec2(0, -10), 10);
+  object obj(vec2(100, 0), vec2(0, -10000/3600), 10);
   obj.name = "rocket";
   sim.add(earth);
   sim.add(obj);
