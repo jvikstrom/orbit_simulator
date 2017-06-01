ef7e6efc6cd0cda41c2e342ad62666ccf0f6fa0b
Johan Vikström <tritzium@gmail.com>
2017-06-01 22:33:07 +0200

    Removed debug statement

--- gui/circle.hpp
+++ gui/circle.hpp
@@ -13,12 +13,10 @@ namespace gui{
     virtual void draw(vec2 position, SDL_Renderer* renderer){
       SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, 255);
       unsigned int r = size.x;
-      std::cout << "Print circle:" << std::endl;
       for(unsigned int x = 0; x < r * 2; x++){
         for(unsigned int y = 0; y < r * 2; y++){
           int dx = r - x, dy = r - y;
           if(dx*dx + dy*dy <= r * r){
-            //std::cout << int(position.x + dx) << ", " << int(position.y + dy) << std::endl;
             SDL_RenderDrawPoint(renderer, position.x + dx, position.y + dy);
           }
         }
